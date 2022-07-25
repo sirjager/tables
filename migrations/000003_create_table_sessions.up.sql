5a080dee51f947e0e2d75f6924c0fba218988b0c
Ankur Kumar <kankurcool@gmail.com>
2022-07-25 10:39:38 +0530

    some naming convention change

--- migrations/000003_create_table_sessions.up.sql
+++ migrations/000003_create_table_sessions.up.sql
@@ -1,23 +1,23 @@
 CREATE TABLE "core_sessions" (
-  "sid" UUID PRIMARY KEY NOT NULL,
-  "uid" BIGINT NOT NULL,
+  "id" UUID PRIMARY KEY NOT NULL,
+  "user_id" BIGINT NOT NULL,
   "client_ip" VARCHAR NOT NULL,
   "user_agent" VARCHAR NOT NULL,
   "refresh_token" VARCHAR NOT NULL,
-  "is_blocked" BOOLEAN NOT NULL DEFAULT FALSE,  
-  "expires_at" TIMESTAMPTZ NOT NULL,
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT (now()),
-  FOREIGN KEY (uid) REFERENCES "public"."core_users" (uid)
+  "blocked" BOOLEAN NOT NULL DEFAULT FALSE,  
+  "expires" TIMESTAMPTZ NOT NULL,
+  "created" TIMESTAMPTZ NOT NULL DEFAULT (now()),
+  FOREIGN KEY (user_id) REFERENCES "public"."core_users" (id)
 );
 
-CREATE INDEX ON "public"."core_sessions" ("uid");
-COMMENT ON COLUMN "core_sessions"."sid" IS 'auto generated session id';
-COMMENT ON COLUMN "public"."core_sessions"."uid" IS 'required,numeric,shouldref=core_users.uid';
+CREATE INDEX ON "public"."core_sessions" ("user_id");
+COMMENT ON COLUMN "core_sessions"."id" IS 'auto generated session id';
+COMMENT ON COLUMN "public"."core_sessions"."user_id" IS 'required,numeric,shouldref=core_users.id';
 COMMENT ON COLUMN "core_sessions"."client_ip" IS 'required,ipaddress';
 COMMENT ON COLUMN "core_sessions"."user_agent" IS 'required,user_agent';
 COMMENT ON COLUMN "core_sessions"."refresh_token" IS 'required,token,desc=refresh token';
-COMMENT ON COLUMN "core_sessions"."is_blocked" IS 'required,boolean,desc=refresh token blocked or not';
-COMMENT ON COLUMN "core_sessions"."expires_at" IS 'timstamp when the refresh token gets expired';
-COMMENT ON COLUMN "core_sessions"."created_at" IS 'timestamp when the refresh token was created';
+COMMENT ON COLUMN "core_sessions"."blocked" IS 'required,boolean,desc=refresh token blocked or not';
+COMMENT ON COLUMN "core_sessions"."expires" IS 'timstamp when the refresh token gets expired';
+COMMENT ON COLUMN "core_sessions"."created" IS 'timestamp when the refresh token was created';
 
 
