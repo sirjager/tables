96597a5b3768f1dca8e6e473fd687984d23c5b58
Ankur Kumar <kankurcool@gmail.com>
2022-07-31 15:36:54 +0530

    .

--- config/config.go
+++ config/config.go
@@ -18,21 +18,14 @@ type ServerConfig struct {
 }
 
 func LoadConfig(envpath string) (config ServerConfig, err error) {
-
 	viper.AddConfigPath(envpath)
-
 	viper.SetConfigName("local")
 	viper.SetConfigType("env")
-
 	viper.AutomaticEnv()
-
 	err = viper.ReadInConfig()
-
 	if err != nil {
 		return
 	}
-
 	err = viper.Unmarshal(&config)
-
 	return
 }
