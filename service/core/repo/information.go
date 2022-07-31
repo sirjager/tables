621f89c285e22828150c85484a50a545668854dd
Ankur Kumar <kankurcool@gmail.com>
2022-07-31 13:10:48 +0530

    seperate info code

--- service/core/repo/information.go
+++ service/core/repo/information.go
@@ -215,3 +215,37 @@ func (q *Queries) CountTablesWithUserID(ctx context.Context, UserID int64) (int6
 	}
 	return count, err
 }
+
+const countUsers = `SELECT COUNT(*) FROM "public"."core_users";`
+
+func (q *Queries) CountUsers(ctx context.Context) (int64, error) {
+	var count int64
+	rows := q.db.QueryRowContext(ctx, countUsers)
+	err := rows.Scan(&count)
+	if err != nil {
+		return 0, err
+	}
+	return count, err
+}
+
+const countUsersWhereBooleanColumnIs = `SELECT COUNT(*) FROM "public"."core_users" WHERE $1 = $2;`
+
+/*This counts any boolean column in users table
+
+boolean columns: "verified" | "blocked" | "public"
+
+example:
+	totalVerifiedUsers,err := CountUsersWhereBooleanColumnIs(context.Background(), "verified", true)
+	totalBlockedUsers,err := CountUsersWhereBooleanColumnIs(context.Background(), "blocked", true)
+	totalPrivateUsers,err := CountUsersWhereBooleanColumnIs(context.Background(), "public", false)
+
+println(count) // int64 value */
+func (q *Queries) CountUsersWhereBooleanColumnIs(ctx context.Context, column string, value bool) (int64, error) {
+	var count int64
+	rows := q.db.QueryRowContext(ctx, countUsersWhereBooleanColumnIs, column, value)
+	err := rows.Scan(&count)
+	if err != nil {
+		return 0, err
+	}
+	return count, err
+}
